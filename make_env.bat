a75f87f1aabacbdde2fe8035cbcf1f1a92292e33
Paul B. Manis <pmanis@med.unc.edu>
2020-10-13 14:13:01 -0400

    windows batch installer appears working

--- make_env.bat
+++ make_env.bat
@@ -1,32 +1,37 @@
-set ENVNAME="cnmodel_venv"
-set cnpath=%cd%
-python3 -m venv $ENVNAME
-C:/%cnpath%/%ENVNAME%/Scripts/activate.bat
-python3 -m pip install --upgrade pip  # be sure pip is up to date in the new env.
-pip3 install wheel  # seems to be missing (note singular)
+Rem @echo off
+set ENVNAME=cnmodel_venv
+Rem CALL %ENVNAME%\Scripts\deactivate.bat
+Rem rmdir /Q /S %ENVNAME% 
+Rem python -m venv %ENVNAME%
+CALL %ENVNAME%\Scripts\activate.bat
+python -m pip install --upgrade pip 
+Rem  be sure pip is up to date in the new env.
+pip3 install wheel  
+Rem seems to be missing (note singular)
 pip3 install cython
-# # if requirements.txt is not present, create:
-# # pip install pipreqs
-# # pipreqs
-#
-# #Then:
-#
+Rem # if requirements.txt is not present, create:
+Rem # pip install pipreqs
+Rem # pipreqs
+
 pip3 install -r requirements.txt
-#source $ENVNAME/bin/activate
-C:/%cnpath%/%ENVNAME%/Scripts/activate.bat
-# build the mechanisms
-# this may equire a separate install of the standard NEURON package
-# with the same version as we have provided
+CALL %ENVNAME%\Scripts\activate.bat
 
-# when first building, it might be a good idea send the nrnivmodl output to files; 
-# Otherwise it can ba harder to find errors in the rest of the installation
-#nrnivmodl cnmodel/mechanisms 1> nrnmodbuild.log 2>nrnmoderror.log
+Rem build the mechanisms
+Rem this may equire a separate install of the standard NEURON package
+Rem with the same version as we have provided
 
-nrnivmodl cnmodel/mechanisms
+Rem when first building, it might be a good idea send the nrnivmodl output to files; 
+Rem Otherwise it can ba harder to find errors in the rest of the installation
+Rem nrnivmodl cnmodel/mechanisms 1> nrnmodbuild.log 2>nrnmoderror.log
+CALL nrnivmodl cnmodel\mechanisms
 
-# these have to be done separately as the compilation depends on what happens above
+Rem these have to be done separately as the compilation depends on what happens above
 pip3 install -e git+https://github.com/pbmanis/cochlea-1.git@c2e8c9612481ebe397ba0e7762b8f2772500388d#egg=cochlea
-rm -rf cochleae
+Rem rm -rf cochleae
 pip3 install -e git+https://github.com/pbmanis/neuronvis.git@Python3#egg=neuronvis
-C:/%cnpath%/%ENVNAME%/Scripts/activate.bat
-python3 setup.py develop
+CALL %ENVNAME%\Scripts\activate.bat
+python setup.py develop
+
+Rem Should always run test afterwards.
+python test.py
+
