465e6b8e9e399c63386131cfeaba3e89ecb906fb
Paul manis <pmanis@med.unc.edu>
2020-11-16 11:32:14 -0500

    adjustments for python3.9 and OSX Big Sur (no open GL yet)

--- make_local_env.sh
+++ make_local_env.sh
@@ -1,18 +1,18 @@
 ENVNAME="cnmodel_venv"
-python3.7 -m venv $ENVNAME
+python3.8 -m venv $ENVNAME
 
 source $ENVNAME/bin/activate
 
-pip install --upgrade pip  # be sure pip is up to date in the new env.
-pip install wheel  # seems to be missing (note singular)
-pip install cython
+pip3 install --upgrade pip  # be sure pip is up to date in the new env.
+pip3 install wheel  # seems to be missing (note singular)
+pip3 install cython
 # # if requirements.txt is not present, create:
 # # pip install pipreqs
 # # pipreqs
 #
 # #Then:
 #
-pip install -r requirements_local.txt
+pip3 install -r requirements_local.txt
 source $ENVNAME/bin/activate
 
 # build the mechanisms
